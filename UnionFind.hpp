55b5cd5e2368ef0cdc05adb7869596d515a74e05
hase0503 <sagaka4446@gmail.com>
2024-04-20 18:13:08 +0900

    add UnionFind

---
+++ UnionFind.hpp
@@ -0,0 +1,21 @@
+struct UnionFind {
+	vector<int> parents;
+
+	UnionFind(int n) : parents(n, -1) {}
+
+	int root(int x) {
+		return parents[x] < 0 ? x : parents[x] = root(parents[x]);
+	}
+
+	void merge(int x, int y) {
+		x = root(x), y = root(y);
+		if (x == y) return;
+		if (-parents[x] < -parents[y]) swap(x, y);
+		parents[x] += parents[y];
+		parents[y] = x;
+	}
+
+	int size(int x) {
+		return -parents[root(x)];
+	}
+};
