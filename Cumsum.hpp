bc9768da94528e3968376448a4b67fd943f1fd22
shu <117558254+hase0503@users.noreply.github.com>
2024-09-08 17:28:42 +0900

    Update Cumsum.hpp

--- Cumsum.hpp
+++ Cumsum.hpp
@@ -1,32 +1,32 @@
 template <typename T>
 struct Cumsum {
-        using F = function<T(T, T)>;
+	using F = function<T(T, T)>;
 
-        int n;
-        vector<T> vct;
+	int n;
+	vector<T> vct;
 
-        T id;
-        F op, inv_op;
+	T id;
+	F op, inv_op;
 
-        Cumsum(int n, T id, F op, F inv_op): n(n), id(id), op(op), inv_op(inv_op) {
-                vct.resize(n + 1, id);
-        }
+	Cumsum(int n, T id, F op, F inv_op): n(n), id(id), op(op), inv_op(inv_op) {
+		vct.resize(n + 1, id);
+	}
 
-        void set(int pos, T x) {
-                vct[pos + 1] = x;
-        }
+	void set(int pos, T x) {
+		vct[pos + 1] = x;
+	}
 
-        void setup() {
-                for (int i = 0; i < n; ++i) {
-                        vct[i + 1] = op(vct[i], vct[i + 1]);
-                }
-        }
+	void setup() {
+		for (int i = 0; i < n; ++i) {
+			vct[i + 1] = op(vct[i], vct[i + 1]);
+		}
+	}
 
-        T get(int pos) {
-                return inv_op(vct[pos + 1], vct[pos]);
-        }
+	T get(int pos) {
+		return inv_op(vct[pos + 1], vct[pos]);
+	}
 
-        T prod(int left, int right) {
-                return inv_op(vct[right], vct[left]);
-        }
+	T prod(int left, int right) {
+		return inv_op(vct[right], vct[left]);
+	}
 };
