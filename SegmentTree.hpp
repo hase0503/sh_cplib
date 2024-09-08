94c46840c2867ac50084e1b931f385ae11dde363
shu <117558254+hase0503@users.noreply.github.com>
2024-09-08 17:22:39 +0900

    Update SegmentTree.hpp

--- SegmentTree.hpp
+++ SegmentTree.hpp
@@ -1,60 +1,86 @@
 template <typename T>
 struct SegmentTree {
-	int n;
-	vector<T> nodes;
-	T ti;
-	function<T(T, T)> op;
+        using F = function<T(T, T)>;
 
-	SegmentTree(int size, T ti, function<T(T, T)> op) : ti(ti), op(op) {
-		n = 1;
-		while (n < size) n *= 2;
-		nodes.resize(n * 2, ti);
-		for (int i = n - 1; i > 0; --i) update(i);
-	}
+        int n;
+        vector<T> nodes;
+        T t_id;
+        F op;
 
-	SegmentTree(vector<T> vec, T ti, function<T(T, T)> op) : ti(ti), op(op) {
-		n = 1;
-		while (n < (int)vec.size()) n *= 2;
-		nodes.resize(n * 2, ti);
-		build(vec);
-	}
+        int bit_width(int x) {
+                int ret = 0;
+                while (x > 0) {
+                        x >>= 1;
+                        ++ret;
+                }
+                return ret;
+        }
 
-	void build(vector<T> vec) {
-		for (int i = 0; i < n; ++i) nodes[n + i] = vec[i];
-		for (int i = n - 1; i > 0; --i) update(i);
-	}
+        SegmentTree(int size, T t_id, F) : t_id(t_id), op(op) {
+                n = 1 << bit_width(size);
+                nodes.resize(n * 2, t_id);
+                for (int i = n - 1; i > 0; --i) {
+                        update(i);
+                }
+        }
 
-	void set(int i, T x) {
-		i += n;
-		nodes[i] = x;
-		while (i /= 2) update(i);
-	}
+        SegmentTree(vector<T> vec, T t_id, function<T(T, T)> op) : t_id(t_id), op(op) {
+                int size = vec.size();
+                n = 1 << bit_width(size);
+                nodes.resize(n * 2, t_id);
+                build(vec);
+        }
 
-	void apply(int i, T x) {
-		set(i, op(nodes[n + i], x));
-	}
+        void build(vector<T> vec) {
+                for (int i = 0; i < int(vec.size()); ++i) {
+                        nodes[n + i] = vec[i];
+                }
+                for (int i = n - 1; i > 0; --i) {
+                        update(i);
+                }
+        }
 
-	T get(int i) {
-		return nodes[n + i];
-	}
+        void set(int i, T x) {
+                i += n;
+                nodes[i] = x;
+                while (i / 2 > 0) {
+                        i /= 2;
+                        update(i);
+                }
+        }
 
-	T prod(int l, int r) {
-		l += n, r += n;
-		T l_cum = ti, r_cum = ti;
-		for (; l < r; l /= 2, r /= 2) {
-			if (l % 2 == 1) l_cum = op(l_cum, nodes[l++]);
-			if (r % 2 == 1) r_cum = op(r_cum, nodes[--r]);
-		}
-		return op(l_cum, r_cum);
-	}
+        void apply(int i, T x) {
+                set(i, op(nodes[n + i], x));
+        }
 
-	T all_prod() {
-		return nodes[1];
-	}
+        T get(int i) {
+                return nodes[n + i];
+        }
+
+        T prod(int l, int r) {
+                l += n, r += n;
+                T l_cum = t_id, r_cum = t_id;
+                while (l < r) {
+                        if (l % 2 == 1) {
+                                l_cum = op(l_cum, nodes[l++]);
+                        }
+                        if (r % 2 == 1) {
+                                r_cum = op(nodes[--r], r_cum);
+                        }
+
+                        l /= 2;
+                        r /= 2;
+                }
+                return op(l_cum, r_cum);
+        }
+
+        T all_prod() {
+                return nodes[1];
+        }
 
 private:
 
-	void update(int x) {
-		nodes[x] = op(nodes[x * 2], nodes[x * 2 + 1]);
-	}
+        void update(int x) {
+                nodes[x] = op(nodes[x * 2], nodes[x * 2 + 1]);
+        }
 };
