60b53c1debddf7547bf29a91ee87d383e533a8ef
shu <117558254+hase0503@users.noreply.github.com>
2024-09-08 17:54:07 +0900

    Update WeightedUnionFind.hpp

--- WeightedUnionFind.hpp
+++ WeightedUnionFind.hpp
@@ -3,27 +3,34 @@ struct WeightedUnionFind {
 	vector<int> parents;
 	vector<T> diff_weights;
 
-	WeightedUnionFind(int n) : parents(n, -1), diff_weights(n) {}
+	WeightedUnionFind(int n) : parents(n, -1), diff_weights(n, 0) {}
 
 	int root(int x) {
-		if (parents[x] < 0) return x;
+		if (parents[x] < 0) {
+			return x;
+		}
 		int root_of_x = root(parents[x]);
 		diff_weights[x] += diff_weights[parents[x]];
 		return parents[x] = root_of_x;
 	}
 
 	void merge(int x, int y, T w) {
-		w += weight(x), w -= weight(y);
+		w -= weight(x), w += weight(y);
 		x = root(x), y = root(y);
-		if (x == y) return;
-		if (-parents[x] < -parents[y]) swap(x, y), w *= -1;
-		parents[x] += parents[y];
-		parents[y] = x;
-		diff_weights[y] = w;
+		if (x == y) {
+			return;
+		}
+		if (-parents[x] > -parents[y]) {
+			swap(x, y);
+			w *= -1;
+		}
+		parents[y] += parents[x];
+		parents[x] = y;
+		diff_weights[x] = w;
 	}
 
-	T diff (int x, int y) {
-		return weight(y) - weight(x);
+	T diff(int x, int y) {
+		return weight(x) - weight(y);
 	}
 
 	bool connected(int x, int y) {
