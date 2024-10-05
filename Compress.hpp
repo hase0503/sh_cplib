945d67b5e1a7ba94438ad08e6a128bd225688e65
shu <117558254+hase0503@users.noreply.github.com>
2024-10-05 17:04:58 +0900

    Update Compress.hpp

--- Compress.hpp
+++ Compress.hpp
@@ -1,12 +1,12 @@
 template<typename T>
-map<T, int> compress(vec<T>& v) {
+map<T, int> compress(vector<T>& v) {
 	map<T, int> cnt;
-	for (int i: range(int(v.size()))) ++cnt[v[i]];
+	for (int i = 0; i < int(v.size()); ++i) ++cnt[v[i]];
 	map<T, int> ret;
 	for (int sum = 0; auto [i, _]: cnt) {
 		ret[i] = sum;
 		sum += cnt[i];
 	}
-	for (int i: range(int(v.size()))) v[i] = ret[v[i]];
+	for (int i = 0; i < int(v.size()); ++i) v[i] = ret[v[i]];
 	return ret;
 }
